eb6a599014badf01b685abb36c287512192369bf
Egor <egor.priemko@protonmail.com>
2023-02-14 13:56:17 +0300

    add root and error pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Root from './pages/Root/Root'
+
 function App() {
-  return <div className="app"></div>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './store/store'
 
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
