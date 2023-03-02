27ae3aa850afa1aacecd61ff828e866550e05eb1
EgorLin <egor.priemko@protonmail.com>
2023-03-02 17:01:02 +0300

    change footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
-function Footer() {
-  return <footer>Footer</footer>
+import { ReactElement } from 'react'
+import styles from './Footer.module.scss'
+
+function Footer(): ReactElement {
+  return <footer className={styles.footer}>Anime App. 2023</footer>
 }
 
 export default Footer
