a4b9d84d47f1f286b33f01fe1b8f0b51ec68d052
EgorLin <egor.priemko@protonmail.com>
2023-03-19 17:29:37 +0300

    update error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,22 @@
-function ErrorPage() {
-  return <div>ErrorPage</div>
+import { ReactElement } from "react";
+import ButtonIcon from "../../components/UI/ButtonIcon/ButtonIcon";
+import styles from "./ErrorPage.module.scss";
+import { RouteNames } from "../../router";
+import { Link } from "react-router-dom";
+
+function ErrorPage(): ReactElement {
+  return (
+    <div className={styles.container}>
+      <div className={styles.window}>
+        <h1>
+          Ups. Error <span className={styles.error404}>404</span>
+        </h1>
+        <Link to={RouteNames.HOME} className={styles.button}>
+          Get back
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
