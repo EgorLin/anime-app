005a5d7f6ef9db7af2aa1fd9b3796d4db4ac4800
EgorLin <egor.priemko@protonmail.com>
2023-03-29 13:11:16 +0300

    add HomeItem

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,41 +1,12 @@
+import { ReactElement } from "react";
 import Catalog from "../../components/Catalog/Catalog";
 import SliderPanel from "../../components/SliderPanel/SliderPanel";
 import ImageCard from "../../components/UI/ImageCard/ImageCard";
 import VideoCard from "../../components/UI/VideoCard/VideoCard";
+import HomeItem from "./HomeItem";
 
-function Home() {
-  return (
-    <>
-      <SliderPanel
-        elements={[
-          <ImageCard id="card-1" />,
-          <ImageCard id="card-1" />,
-          <ImageCard id="card-1" />,
-          <ImageCard id="card-1" />,
-          <ImageCard id="card-1" />,
-          <ImageCard id="card-1" />,
-        ]}
-      />
-      <Catalog
-        title="Recent releases"
-        elements={[
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-          <VideoCard />,
-        ]}
-      />
-    </>
-  );
+function Home(): ReactElement {
+  return <HomeItem />;
 }
 
 export default Home;
