9f220f714a4d8141b4f81d28731873ecf19a814f
EgorLin <egor.priemko@protonmail.com>
2023-04-04 11:08:33 +0300

    add useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,25 @@
+import { useCallback, useRef } from "react";
+
+function useDebounce(
+  callback: (...args: any[]) => void,
+  delay: number
+): ReturnType<typeof useCallback> {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+
+  const debounceCallback = useCallback(
+    (...args: any[]) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, 500);
+    },
+    [callback, delay]
+  );
+
+  return debounceCallback;
+}
+
+export default useDebounce;
